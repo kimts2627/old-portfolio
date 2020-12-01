1ced2663a61d336c74c1b2e6dabc9fe6507f1fff
kimts2627 <kimtstststs@gmail.com>
2020-12-01 18:22:23 +0900

    add custome cursor, folio transition function

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,12 +3,15 @@ import React from 'react';
 import  Sidebar from './Sidebar';
 import  Main from './Main';
 import  Title from './Title';
+import MyImg from './images/IMG_1786.jpeg';
+import TodoImg from './images/todo.png';
 import '../App.css';
 
 interface Props {};
 interface State {
   isSidebarOn: boolean,
-  portfolios: Array<object>
+  portfolios: Array<object>,
+  currentNum: number
 };
 
 class App extends React.Component<Props, State> {
@@ -22,29 +25,65 @@ class App extends React.Component<Props, State> {
           "title": "Todo List App",
           "stack": "HTML/CSS/React/JS",
           "detail": "JSX의 기본 문법을 활용한 Todo App 입니다.\n다양한 디바이스를 통해 이용 가능하게 3단 레이아웃 반응형으로 제작 하였습니다.",
-          "pic": "./images/todo.png"
+          "pic": MyImg
         },
         {
           "id": 2,
           "title": "Todo List App",
           "stack": "HTML/CSS/React/JS",
           "detail": "JSX의 기본 문법을 활용한 Todo App 입니다.\n다양한 디바이스를 통해 이용 가능하게 3단 레이아웃 반응형으로 제작 하였습니다.",
-          "pic": "images/todo.png"
+          "pic": TodoImg
+        },
+        {
+          "id": 3,
+          "title": "Todo List App",
+          "stack": "HTML/CSS/React/JS",
+          "detail": "JSX의 기본 문법을 활용한 Todo App 입니다.\n다양한 디바이스를 통해 이용 가능하게 3단 레이아웃 반응형으로 제작 하였습니다.",
+          "pic": MyImg
         }
-      ]
+      ],
+      currentNum: 0
     }
     this.handleProfileClick = this.handleProfileClick.bind(this);
+    this.handleCountPlusClick = this.handleCountPlusClick.bind(this);
+    this.handleCountMinusClick = this.handleCountMinusClick.bind(this);
+  }
+
+  componentDidUpdate() {
+    
   }
 
   private handleProfileClick(): void {
-    this.setState({ isSidebarOn: !this.state.isSidebarOn })
+    this.setState({ isSidebarOn: !this.state.isSidebarOn });
+  }
+
+  private handleCountPlusClick(): void {
+    if(this.state.currentNum < this.state.portfolios.length - 1) {
+      this.setState(preState => ({
+        currentNum: preState.currentNum + 1 
+      }));
+    }
+  }
+
+  private handleCountMinusClick(): void {
+    if(this.state.currentNum > 0) {
+      this.setState(prevState => ({
+        currentNum: prevState.currentNum - 1
+      }));
+    }
   }
 
   public render() {
     return (
       <div className='App'>
         <Sidebar isSidebarOn={this.state.isSidebarOn} handleProfileClick={this.handleProfileClick}/>
-        <Main portfolios={this.state.portfolios}/>
+        <Main
+        portfolios={this.state.portfolios}
+        currentNum={this.state.currentNum}
+        isSidebarOn={this.state.isSidebarOn}
+        handleCountPlusClick={this.handleCountPlusClick}
+        handleCountMinusClick={this.handleCountMinusClick}
+        />
         <Title />
         <div className="Title-power">Powered By React and TypeScript</div>
         
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,20 +2,24 @@ import React from 'react';
 import ImgSrc from './images/IMG_1786.jpeg';
 
 interface MainProps {
-  portfolios: Array<object>
+  portfolios: Array<object | any>,
+  currentNum: number,
+  isSidebarOn: boolean,
+  handleCountPlusClick: () => void,
+  handleCountMinusClick: () => void
 }
 
 function Main(props: MainProps) {
-  const {portfolios} = props;
+  const {portfolios, currentNum, isSidebarOn, handleCountPlusClick, handleCountMinusClick} = props;
   return (
     <main className="main-container">
       <div className="dummy">
-        <img src={ImgSrc} alt=""/>
+        <img src={portfolios[currentNum].pic} alt=""/>
       </div>
-      <div className="main-left"/>
-      <div className="main-right"/>
+      <div className="main-left" onClick={isSidebarOn ? undefined : handleCountMinusClick}/>
+      <div className="main-right" onClick={isSidebarOn ? undefined : handleCountPlusClick}/>
       <footer className="scrollbar">
-        {portfolios.map((el: any) => <p className="scrollbar-el" key={el.id}>I</p>)}
+        {portfolios.map((el: any, index: number) => index === currentNum ? <p className="scrollbar-el-up" key={el.id}>I</p> : <p className="scrollbar-el" key={el.id}>I</p>)}
       </footer>
     </main>
   )
