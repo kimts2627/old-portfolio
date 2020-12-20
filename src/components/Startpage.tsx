9d74f10ee0887a4219e6383bf09b7e7eab8682a4
kimts2627 <kimtstststs@gmail.com>
2020-12-20 19:06:43 +0900

    add startpage desolve effect

--- src/components/Startpage.tsx
+++ src/components/Startpage.tsx
@@ -2,23 +2,35 @@ import React from 'react';
 
 interface Startprops {};
 interface Startstate {
-
+  isBlank: boolean
 };
 
 class Startpage extends React.Component<Startprops, Startstate> {
   constructor(props: Startprops) {
-    super(props)
+    super(props);
+    this.state = {
+      isBlank: false
+    };
+    this.deleteAll = this.deleteAll.bind(this);
   }
 
+  componentDidMount() {
+    setTimeout(this.deleteAll, 2000);
+  }
 
+  public deleteAll() {
+    this.setState({
+      isBlank: true
+    });
+  }
 
   public render() {
     return (
-      <div className="Start-wrap">
+      <div className={this.state.isBlank ? "Start-wrap-blank" : "Start-wrap"}>
         <h1 className="Start-name">Kim Taesu</h1>
         <p className="Start-desc">Frontend Developer</p>
       </div>
-    );
+    )
   }
 }
 
